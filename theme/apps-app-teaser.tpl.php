04ac1bcdfe24956bab260d11c3a75c87d30c0efa
e2thex <erik@e2thex.com>
2014-08-11 17:36:25 +0000

    issue #2317283: ajusted config link to be on the right

--- theme/apps-app-teaser.tpl.php
+++ theme/apps-app-teaser.tpl.php
@@ -8,9 +8,9 @@
     <h2><?php print $name ?></h2>
     <div class="app-status"><?php print $status ?></div>
     <?php print drupal_render($rating); ?>
+    <div class="app-action"><?php print $action; ?></div>
     <?php if ($config): ?>
       <div class="app-action"><?php print $config; ?></div>
     <?php endif; ?>
-    <div class="app-action"><?php print $action; ?></div>
   </div>
 </div>
